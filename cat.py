088b2d34bcb8cfb3e72ccd2ebad1f4a8deb4a191
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-20 14:35:23 -0700

    more examples to print multiple times

--- cat.py
+++ cat.py
@@ -1,4 +1,9 @@
-i = 3
-while i != 0:
-    print("meow")
-    i = i - 1
+# i = 0
+# while i <= 3:
+#     print("meow")
+#     i += 1
+
+# for _ in range(10):
+#     print("meow")
+
+print("meow\n" * 3, end="")
