1f1b350c819d3dcbedb12b23a3e597e09072a72d
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-20 11:05:12 -0700

    refactored code and added a square function

--- calculator.py
+++ calculator.py
@@ -1,6 +1,15 @@
-x = float(input("What's x? "))
-y = float(input("What's z? "))
+def main():
+    x = float(input("What's x? "))
+    y = float(input("What's z? "))
 
-z = round(x + y)
+    z = round(x + y)
 
-print(f"{z:,}")
+    print("The addition of x and y is " f"{z:,}")
+    print ("x squared is", square(x))
+
+def square(n):
+    # return n ** 2
+    return pow(n, 2)
+
+main()
+
