a0b0f67d6ec1ade1609f0e337671ed237bb8d07b
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-20 16:29:22 -0700

    handling errors

--- number.py
+++ number.py
@@ -1,8 +1,14 @@
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("x is not an integer")
-else:
+def main():
+    x = get_int("What's x? ")
     print(f"x is {x}")
-except ValueError:
-    print("x is not an integer")
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("x is not an integer")
+
+main()
+
+
