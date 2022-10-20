be5d5031b205ca3f79b7e0a5d3843f0059658cbf
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-20 15:01:45 -0700

    looping over dictionaries

--- hogwarts.py
+++ hogwarts.py
@@ -1,7 +1,23 @@
-students = ["Hermione", "Harry", "Ron"]
+# students = ["Hermione", "Harry", "Ron", "Draco"]
+# houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
 
 # for student in students:
 #     print(student)
 
-for i in range(len(students)):
-    print(i + 1, students[i])
+# for i in range(len(students)):
+#     print(i + 1, students[i])
+
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin"
+}
+
+# print(students["Hermione"])
+# print(students["Harry"])
+# print(students["Ron"])
+# print(students["Draco"])
+
+for student in students:
+    print(student, students[student], sep=", ")
