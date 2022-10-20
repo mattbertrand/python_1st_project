c212dd02a05ef54e21bd50a9d5fe43cc8c34e615
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-20 13:27:02 -0700

    refactored

--- parity.py
+++ parity.py
@@ -13,9 +13,13 @@ def main():
         print("Odd")
 
 def is_even(n):
-    if n % 2 == 0:
-        return True
-    else:
-        return False
+    # if n % 2 == 0:
+    #     return True
+    # else:
+    #     return False
+
+    # return True if n % 2 == 0 else False
+
+    return n % 2 == 0
 
 main()
