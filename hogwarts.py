8ff7b1ac7a3689cf9432b32e125977f3fd7a8d43
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-20 15:12:29 -0700

    dictionaries with keys and values

--- hogwarts.py
+++ hogwarts.py
@@ -7,17 +7,27 @@
 # for i in range(len(students)):
 #     print(i + 1, students[i])
 
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin"
-}
+# students = {
+#     "Hermione": "Gryffindor",
+#     "Harry": "Gryffindor",
+#     "Ron": "Gryffindor",
+#     "Draco": "Slytherin"
+# }
 
 # print(students["Hermione"])
 # print(students["Harry"])
 # print(students["Ron"])
 # print(students["Draco"])
 
+# for student in students:
+#     print(student, students[student], sep=", ")
+
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
+]
+
 for student in students:
-    print(student, students[student], sep=", ")
+    print(student["name"], student["house"], student["patronus"], sep=", ")
