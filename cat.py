15b0e86bcd853e1022618b1a965c5bb11f6d1dd2
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-20 14:41:27 -0700

    asking user for input to print

--- cat.py
+++ cat.py
@@ -6,4 +6,29 @@
 # for _ in range(10):
 #     print("meow")
 
-print("meow\n" * 3, end="")
+# print("meow\n" * 3, end="")
+
+# while True:
+#     n = int(input("What's n? "))
+#     if n > 0:
+#         break
+
+# for _ in range(n):
+#     print("meow")
+
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+    return n
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+main()
