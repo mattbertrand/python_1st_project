c188ce0e9f3238c296ab90e5dc29b4d7c98a45fe
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-21 09:15:49 -0700

    examples of radint and shuffle

--- generate.py
+++ generate.py
@@ -1,5 +1,12 @@
 import random
 
-coin = random.choice(["heads", "tails"])
+# coin = random.choice(["heads", "tails"])
+# print(coin)
 
-print(coin)
+# number = random.randint(1, 10)
+# print(number)
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
