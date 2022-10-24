62045a16ef96ba0ad2aa319c90f0b7268ffd498b
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-24 11:50:11 -0700

    refactored

--- students.py
+++ students.py
@@ -1,4 +1,9 @@
+# with open("students.csv") as file:
+#     for line in file:
+#         row = line.rstrip().split(",")
+#         print(f"{row[0]} is in {row[1]}")
+
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        name, house = line.rstrip().split(",")
+        print(f"{name} is in {house}")
