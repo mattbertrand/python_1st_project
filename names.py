c597633cab1eac860946b02a30fe88c332be1671
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-24 11:24:43 -0700

    append the names in a new txt file

--- names.py
+++ names.py
@@ -1,7 +1,12 @@
-names= []
+# names= []
 
-for _ in range(3):
-    names.append(input("What's your name? "))
+# for _ in range(3):
+#     names.append(input("What's your name? "))
 
-for name in sorted(names):
-    print(f"hello, {name}")
+# for name in sorted(names):
+#     print(f"hello, {name}")
+
+name = input("What's your name? ")
+
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
