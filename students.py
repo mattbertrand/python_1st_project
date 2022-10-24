bb60d5703bd61d1b781b47054aea79463d01f61f
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-24 16:18:41 -0700

    refactored

--- students.py
+++ students.py
@@ -3,7 +3,21 @@
 #         row = line.rstrip().split(",")
 #         print(f"{row[0]} is in {row[1]}")
 
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         print(f"{name} is in {house}")
+
+students = []
+
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+# def get_name(student):
+#     return student["name"]
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['house']}")
