6cbffaac6c5a138e290daa424badb2e337303181
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-25 16:20:34 -0700

    forgot to save

--- validate.py
+++ validate.py
@@ -12,7 +12,7 @@ import re
 email = input("What's your email? ").strip()
 
 # if re.search(r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.com$", email):
-if re.search(r"^\w+@(\w+\.)?\w+\.(com|gov|net|org)$", email, re.IGNORECASE):
+if re.search(r"^(\w|\.)+@(\w+\.)?\w+\.(com|gov|net|org)$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
