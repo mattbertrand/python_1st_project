4939d894490fd46ac0208a5d55408027b98e3afa
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-25 14:34:25 -0700

    used a reader

--- students.py
+++ students.py
@@ -8,16 +8,28 @@
 #         name, house = line.rstrip().split(",")
 #         print(f"{name} is in {house}")
 
-students = []
+# students = []
 
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         student = {"name": name, "house": house}
+#         students.append(student)
 
 # def get_name(student):
 #     return student["name"]
 
+# for student in sorted(students, key=lambda student: student["name"]):
+#     print(f"{student['name']} is in {student['house']}")
+
+import csv
+
+students = []
+
+with open("students.csv") as file:
+    reader = csv.reader(file)
+    for name, home in reader:
+        students.append({"name": name, "home": home})
+
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is in {student['home']}")
