0fdd79cb59e976f7d30b7d8d78775bda16b54aca
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-25 14:48:48 -0700

    used DictWriter

--- students.py
+++ students.py
@@ -22,14 +22,25 @@
 # for student in sorted(students, key=lambda student: student["name"]):
 #     print(f"{student['name']} is in {student['house']}")
 
+# import csv
+
+# students = []
+
+# with open("students.csv") as file:
+#     reader = csv.reader(file)
+#     for name, home in reader:
+#         students.append({"name": name, "home": home})
+
+# for student in sorted(students, key=lambda student: student["name"]):
+#     print(f"{student['name']} is in {student['home']}")
+
+# Prompting the users
+
 import csv
 
-students = []
+name = input("What's your name? ")
+home = input("Where's your home? ")
 
-with open("students.csv") as file:
-    reader = csv.reader(file)
-    for name, home in reader:
-        students.append({"name": name, "home": home})
-
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['home']}")
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
