a6881aacc70fa66710f32123d2efcbb397fdabb7
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-26 10:57:12 -0700

    adding a new attribute

--- oop/student.py
+++ oop/student.py
@@ -6,17 +6,35 @@ class Student:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        # self.patronus = patronus
+    
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    # match not available with this version of Python
+    # def charm(self):
+    #     match self.patronus:
+    #         case "Stag":
+    #             return "🐴"
+    #         case "Otter":
+    #             return "🦫"
+    #         case "Jack Russel terrier":
+    #             return "🐶"
+    #         case _:
+    #             return "🪄"
 
 def main():
     student = get_student()
     # if student["name"] == "Padma":
     #     student["house"] = "Ravenclaw"
-    print(f"{student.name} from {student.house}")
+    # print(f"{student.name} from {student.house}")
+    print(student)
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
+    # patronus = input("Patronus: ")
     return Student(name, house)
 
 if __name__ == "__main__":
