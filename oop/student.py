ccf080b7d93f0065e3490643ef10ee57d310c4da
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2022-10-26 10:33:53 -0700

    raise excpetions when wrong input

--- oop/student.py
+++ oop/student.py
@@ -1,8 +1,9 @@
-from unicodedata import name
-
-
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
         self.name = name
         self.house = house
 
@@ -16,8 +17,7 @@ def main():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    student = Student(name, house)
-    return student
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
